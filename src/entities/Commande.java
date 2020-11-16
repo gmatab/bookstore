f430b75d051efae14837ddb75572a4778a3206d4
Tuxette Chix <tuxette@chix.nathalievilla.org>
2020-11-16 21:26:06 +0100

    Amen's branch STEP3

---
+++ src/entities/Commande.java
@@ -0,0 +1,61 @@
+package entities;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+public class Commande {
+	private int id;
+	private Date date_commande;	
+	private Double prix;
+	private ArrayList <Book> lb;
+	private int idClient;
+	private int idBook ;
+
+	
+
+
+		public Commande(int id, Date date_commande, Double prix,int idClient, int idBook) {
+		this.id = id;
+		this.date_commande = date_commande;
+		this.prix = prix;
+		this.idClient = idClient;
+		this.idBook = idBook;
+	}
+
+		public int getIdBook() {
+		return idBook;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Date getDate_commande() {
+		return date_commande;
+	}
+	public void setDate_commande(Date date_commande) {
+		this.date_commande = date_commande;
+	}
+	public Double getPrix() {
+		return prix;
+	}
+	public void setPrix(Double prix) {
+		this.prix = prix;
+	}
+ 
+	public int getIdClient() {
+		return idClient;
+	}
+	public void setIdClient(int idClient) {
+		this.idClient = idClient;
+	}
+ 	@Override
+public String toString() {
+	return "Commande [id=" + id + ", date_commande=" + date_commande + ", prix=" + prix 
+			+ ", idClient=" + idClient + ", idBook=" + idBook + "]";
+}
+	
+
+}
--- src/gui/Grid.java
+++ src/gui/Grid.java
@@ -141,7 +141,7 @@ public class Grid extends JFrame {
 		int j=books.size();
 		Object[][] data = new Object[j][6];
 		int i=0;
-		ImageIcon icon =new ImageIcon("C:\\Users\\Amen\\eclipse-workspace\\bookstore\\src\\gui\\iconb.png");
+		ImageIcon icon =new ImageIcon("C:\\Users\\Amen\\eclipse-workspace\\bookstore\\src\\gui\\book.gif");
 		for(Book bo : books)
 	    {
 			System.out.println(bo.getId());
